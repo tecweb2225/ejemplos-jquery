e8dd346f0e5afee3fe9621e9a888f23e274ee25a
eloyhz <eloyhz@gmail.com>
2017-03-08 22:19:25 -0600

    Primer ejemplo de jQuery.

---
+++ holajquery.js
@@ -0,0 +1,13 @@
+// $(document).ready(saluda)
+
+// function saluda()   {
+//     alert($('h1').text())
+// }
+
+$(function(){
+//    alert($('h1').text())
+    // se crea un parrafo nuevo 
+    // en un objeto jQuery
+    $nuevo = $('<p>Parrafo <b>nuevo</b></p>')
+    $('h2').after($nuevo)
+})
