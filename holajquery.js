ac717c52f89aef05b57103003623a0adf7002645
eloyhz <eloyhz@gmail.com>
2017-03-15 08:31:28 -0600

    Ejemplo de efectos y animaciones en jQuery

--- holajquery.js
+++ holajquery.js
@@ -4,10 +4,19 @@
 //     alert($('h1').text())
 // }
 
+/*
 $(function(){
 //    alert($('h1').text())
     // se crea un parrafo nuevo 
     // en un objeto jQuery
     $nuevo = $('<p>Parrafo <b>nuevo</b></p>')
     $('h2').after($nuevo)
+    $('#boton1').click(function (e) { 
+        e.preventDefault();
+        
+    });
+    $(selector).on(events, function () {
+        
+    });
 })
+*/
